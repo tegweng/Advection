36d8bc15feb6605aa0aba9f9314fe74473c94fa6
tegweng <32457042+tegweng@users.noreply.github.com>
2017-11-07 15:55:10 +0000

    Adding file for BTBS
    Not finished, just want it on GitHub

---
+++ advectionBTBS.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 07 14:08:55 2017
+
+@author: 25803263
+"""
+
+# BTBS scheme for simulating advection for outer code advection.py
+
+from __future__ import absolute_import, division, print_function
+import numpy as np
+
+# The linear algebra package for BTBS (for solving the matrix equation)
+import scipy.linalg as la
+
+
+def BTBS(phi, c, nt):
+    "Diffusion of profile in phi using BTBS using the\
+    Courant number, c, assuming fixed value boundary conditions"
+    
+    nx = len(phi)
+    
+    #array representing BTBS
+    M=np.zeros([nx,nx])
+    
+    #zero gradient boundary conditions
+    
+    M[0,0] = 1.
+    M[0,1] = -1.
+    M[-1,-1] = 1.
+    M[-1,-2] = -1.
+    
+    for i in xrange(1,nx-1):
+        M[i,i-1] = -d
+        M[i,i] = 1+2*d
+        M[i,i+1] = -d
+    
+    #BTBS for all time steps
+    
+    for it in xrange(int(nt)):
+        #RHS for zero gradient boundary conditions - have to keep resetting the \
+        #boundary to be zero each time step
+        phi[0] = 0
+        phi[-1] = 0
+        #as have to do the following for all time steps it is in the for loop
+        phi = la.solve(M, phi)
+    
+    return phi
