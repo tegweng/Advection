349c23d7eafb395d8921cde05916d5aa3678090f
LaurenHop22 <32457249+LaurenHop22@users.noreply.github.com>
2017-11-07 15:47:23 +0000

    Add files via upload

---
+++ Advection_FTBS.py
@@ -0,0 +1,26 @@
+# Student ID: fr818629
+# FTBS numerical scheme used to solve the advection equation
+
+from __future__ import absolute_import, division, print_function
+import numpy as np
+
+#Define a function to solve advection equation using FTBS
+def FTBS(phiOld, c, nt):
+    '''
+    Advection of profile in phiOld using the Courant number, c.
+    '''
+    
+    #Number of space steps is the length of the orifginal phi
+    nx = len(phiOld)
+    
+    #new time-step array for phi
+    phi = phiOld.copy()
+    
+    #FTBS for all time steps using arrays over time and space
+    #Using modulo arithmetic for periodic boundaries
+    for it in xrange(nt):
+        for i in xrange(1, nx-1):
+            phi[i] = phiOld[i] - c*(phiOld[i] - phiOld[(i-1)%nx])
+        phiOld = phi.copy()
+        
+    return phi
