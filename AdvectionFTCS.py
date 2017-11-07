8006c2d1b273b63d7870e2c658e900c34e12ba7a
zq806676 <32457387+zq806676@users.noreply.github.com>
2017-11-07 15:47:43 +0000

    AdvectionFTCS

---
+++ AdvectionFTCS.py
@@ -0,0 +1,19 @@
+# Numerical schemes for simulating advection.py
+
+from __future__ import absolute_import, division, print_function
+import numpy as np
+
+
+def FTCS(phiOld, c, nt):
+    "Advection of profile in phiOld using FTCS using non-dimensional"
+    "advection coeffient, c"
+    
+    nx = len(phiOld)
+    
+    phi = phiold.copy()
+    
+    for i in xrange(nx):
+        phi[i] = phiold[i] - 0.5*c*(phiold[i+1] - phiold[(i-1)%nx])
+        
+    return phi
+
