690cd7a6a9a7599011f633f64c3f79258d6f69e5
tegweng <32457042+tegweng@users.noreply.github.com>
2017-11-14 15:03:32 +0000

    TVD second attempt

--- advectionTVD.py
+++ advectionTVD.py
@@ -10,29 +10,29 @@ Created on Tue Nov 14 14:16:39 2017
 from __future__ import absolute_import, division, print_function
 import numpy as np
 
-def TVD(phi, c, nt, u):
+def TVD(phiOld, c, nt, u):
     
-    nx = len(phi)
+    nx = len(phiOld)
     #Lax-Wendroff as the high-order flux
     phiH = np.zeros(nx)
     
     for i in xrange(0,nx):
-        phiH[i] = 0.5 * (1 + c) * phi[i] +0.5 * (1 - c) * phi[(i+1)%nx]
+        phiH[i] = 0.5 * (1 + c) * phiOld[i] +0.5 * (1 - c) * phiOld[(i+1)%nx]
     
     #First-order upwinds as the low-order flux
     phiL = np.zeros(nx)
     
     for i in xrange(0, nx):    
         if u < 0:
-            phiL[i] = phi[i]
+            phiL[i] = phiOld[i]
         else: 
-            phiL[i] = phi[(i+1)%nx]
+            phiL[i] = phiOld[(i+1)%nx]
     
     #calculating r
     r = np.zeros(nx) 
     
     for i in xrange(0, nx):
-        r[i] = (phi[i] - phi [(i-1)%nx]) / (phi[(i+1)%nx] - phi[i])
+        r[i] = (phiOld[i] - phiOld[(i-1)%nx]) / (phiOld[(i+1)%nx] - phiOld[i])
     
     #calculating Van Leer Limiter    
     VLL = np.zeros(nx)
@@ -41,7 +41,12 @@ def TVD(phi, c, nt, u):
         VLL[i] = ( r[i] + abs(r[i]) ) / (1 + abs(r[i]) )
     
     #calculating new value of phi
+    phi = np.zeros(0,nx)
     for i in xrange(0, nx):
         phi[i] = VLL[i] * phiH[i] + (1 - VLL[i]) * phiL[i]
+        
+    #calculating the solution after so many time steps
+    
+    
     
     return phi
