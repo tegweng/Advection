bb9e638d7e6debaa40dff4d54ad878c4ccba1d5b
zq806676 <32457387+zq806676@users.noreply.github.com>
2017-11-20 08:59:24 +0800

    Update AdvectionFTCS.py

--- AdvectionFTCS.py
+++ AdvectionFTCS.py
@@ -8,8 +8,8 @@ id 25806676
 """
 
 def FTCS(phiOld, c, nt):
-"Advection of profile in phiOld using FTCS using non-dimensional"
-"advection Courant number, c = u*dt/dx"
+    "Advection of profile in phiOld using FTCS"
+    "advection Courant number, c = u*dt/dx"
     
     #arguement tesing
     if nt <= 0:
