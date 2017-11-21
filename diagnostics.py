028ca57ca7bb6a969ee55a163fb635469321ae48
tegweng <32457042+tegweng@users.noreply.github.com>
2017-11-21 15:47:49 +0000

    adding a dispersion error plot
    not quite right yet - need to talk to hillary

--- diagnostics.py
+++ diagnostics.py
@@ -11,8 +11,10 @@ from __future__ import absolute_import, division, print_function
 ### The numpy package for numerical functions and pi                ###
 import numpy as np
 
-# Import the special package for the erf function
-from scipy import special
+import math
+
+#import package for graphing
+import matplotlib.pyplot as plt
 
 
 def L2ErrorNorm(phi, phiExact):
@@ -28,4 +30,73 @@ def L2ErrorNorm(phi, phiExact):
     L2 = np.sqrt(sum(phiError**2)/sum(phiExact**2))
 
     return L2
+    
+def disprel(c, dx):
+    """
+    25803263
+    Function to calculate the dispersion relation for CTCS and plot 
+    the relation with the w = k. const the actual solution. u = 1.
+    """
+    k = np.arange(0, int(np.pi / dx)+1, 1)
+    kdx = k * dx
+    ang = np.zeros(len(k))
+    cang = np.zeros(len(k))
+    alpha = np.zeros(len(k))
+    pmode = np.zeros(len(k))
+    cmode = np.zeros(len(k))
+    tmode = np.zeros(len(k))
+    
+    for i in xrange(0,int(np.pi / dx)+1):
+        alpha[i] = math.asin(c * math.sin(kdx[i]))
+         
+        if alpha[i] == 0:
+            pmode[i] = 1
+        else:
+            pmode[i] = alpha[i] / (c * kdx[i])
+    
+        cmode[i] = - pmode[i]
+    
+        tmode[i] = kdx[i]
+        
+        ang[i] = float(kdx[i]) * pmode[i]
+        cang[i] = -ang[i]
+    
+    font = {'size' : 10}
+    plt.rc('font', **font)
+    plt.figure(1)
+    plt.clf()
+    plt.ion()
+    plt.plot(kdx, pmode, label='Physical mode', color='black')
+    plt.plot(kdx, cmode, label='Computational mode', color='black')
+    plt.axhline(0, linestyle=':', color='black')
+    plt.xlim([0,np.pi])
+    plt.ylim([-1.5,1.5])
+    plt.xticks
+    plt.legend()
+    plt.xlabel('$k\Delta x$')
+    plt.ylabel('$u_n/u$')
+    plt.title("dx = 0.01 c = 0.128")
+    plt.savefig('Plots/dispersionrelation.pdf')
+    
+    font = {'size' : 10}
+    plt.rc('font', **font)
+    plt.figure(2)
+    plt.clf()
+    plt.ion()
+    plt.plot(kdx, ang, label='Physical mode', color='black')
+    plt.plot(kdx, tmode, label='True', color='black', linestyle='--', \
+             linewidth=2)
+    plt.plot(kdx, cang, label='Computational mode', color='black')
+    plt.axhline(0, linestyle=':', color='black')
+    plt.xlim([0,np.pi])
+    plt.ylim([-1.5,1.5])
+    plt.xticks
+    plt.legend()
+    plt.xlabel('$k\Delta x$')
+    plt.ylabel('$\omega_n/u$')
+    plt.title("dx = 0.01 c = 0.128")
+    plt.savefig('Plots/dispersionrelation2.pdf')
+
+    
+    
 
