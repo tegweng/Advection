d42aaed844d4db64bc731cd98bc118c13eaa6656
and1994 <and.199four@gmail.com>
2017-11-26 16:11:29 +0000

    New parts in my schemes

--- Artificial_diffusion.py
+++ Artificial_diffusion.py
@@ -8,7 +8,19 @@ Code for implementing artificial diffusion into the linear advection scheme.
 from __future__ import absolute_import, division, print_function
 import numpy as np
 
-
+def TV(phi):
+    """
+    Code for computing Total Variation for a function phi
+    (with periodic boundaries).
+    """
+    if not(isinstance(phi,np.ndarray)):
+        raise TypeError('Error in TV: Argument phi to TV should be an array')
+        
+    nx = len(phi)    
+    I=0.
+    for i in range(nx):
+        I+=abs(phi[(i+1)%nx]-phi[i])
+    return I
 
 def Artificial_diffusion(phiOld, c, nt, dx, dt, d, orderAD=2):
 
@@ -40,7 +52,7 @@ def Artificial_diffusion(phiOld, c, nt, dx, dt, d, orderAD=2):
     if not(isinstance(float(dt),float) and float(dt) > 0):
         raise TypeError('Error in Artificial_diffusion:\
                 Argument dt to Artificial_diffusion should be a positive float')
-    if not(isinstance(float(d),float) and float(d) > 0):
+    if not(isinstance(float(d),float) and float(d) >= 0):
         raise TypeError('Error in Artificial_diffusion:\
                 Argument d to Artificial_diffusion should be a positive float')
     if not(isinstance(phiOld,np.ndarray)):
@@ -55,6 +67,8 @@ def Artificial_diffusion(phiOld, c, nt, dx, dt, d, orderAD=2):
     # new time-step array for phi
     phiNew = phiOld.copy()
     phiCurrent = phiOld.copy()
+    # initialising Total Variation for monotonicity analysis
+    TotalVariation = np.zeros(nt)
     
     if (orderAD==2):
         # FTCS for both advection and diffusion to compute the first time step
@@ -62,13 +76,21 @@ def Artificial_diffusion(phiOld, c, nt, dx, dt, d, orderAD=2):
             phiCurrent[j] = phiOld[j] - c*0.5*(phiOld[(j+1)%nx] - \
                       phiOld[(j-1)%nx]) + d*(phiOld[(j+1)%nx] - 2*phiOld[j]\
                              + phiOld[(j-1)%nx])
+        
+        # total variation for first time step
+        TotalVariation[0]=TV(phiCurrent)
+        
         # CTCS for advection, FTCS for diffusion
-        for it in range(nt-1):
+        for it in range(1,nt):
             # spatial points
             for j in range(0,nx):
                 phiNew[j] = phiOld[j] - \
                 c*(phiCurrent[(j+1)%nx] - phiCurrent[(j-1)%nx]) + \
                 2*d*(phiOld[(j+1)%nx] - 2*phiOld[j] + phiOld[(j-1)%nx])
+            
+            # total variation for time step 2 to nt
+            TotalVariation[it]=TV(phiNew)
+            
             # output to phiOld for the next time-step
             phiOld = phiCurrent.copy()
             phiCurrent = phiNew.copy()
@@ -80,14 +102,22 @@ def Artificial_diffusion(phiOld, c, nt, dx, dt, d, orderAD=2):
                       - phiOld[(j-1)%nx]) - d*(phiOld[(j+2)%nx]\
                                - 4*phiOld[(j+1)%nx] + 6*phiOld[j]\
                                - 4*phiOld[(j-1)%nx] + phiOld[(j-2)%nx])
+            
+        # total variation for first time step
+        TotalVariation[0]=TV(phiCurrent)
+        
         # CTCS for advection, FTCS for diffusion
-        for it in range(nt-1):
+        for it in range(1, nt):
             # spatial points
             for j in range(0,nx):
                 phiNew[j] = phiOld[j] - \
                 c*(phiCurrent[(j+1)%nx] - phiCurrent[(j-1)%nx]) - \
                 2*d*(phiOld[(j+2)%nx] - 4*phiOld[(j+1)%nx] + \
                             6*phiOld[j] - 4*phiOld[(j-1)%nx] + phiOld[(j-2)%nx])
+            
+            # total variation for time step 2 to nt
+            TotalVariation[it]=TV(phiNew)
+            
             # output to phiOld for the next time-step
             phiOld = phiCurrent.copy()
             phiCurrent = phiNew.copy()
@@ -97,4 +127,4 @@ def Artificial_diffusion(phiOld, c, nt, dx, dt, d, orderAD=2):
             'Error in Artificial_diffusion:\
            Argument orderAD to Artificial_diffusion should be 2 or 4 (integer)')
 
-    return phiNew, k
+    return phiNew, k, TotalVariation
--- advectionCTCS.py
+++ advectionCTCS.py
@@ -3,7 +3,20 @@
 from __future__ import absolute_import, division, print_function
 import numpy as np
 
-
+def TV(phi):
+    """
+    Code for computing Total Variation for a function phi
+    (with periodic boundaries).
+    """
+    if not(isinstance(phi,np.ndarray)):
+        raise TypeError('Error in TV: Argument phi to TV should be an array')
+        
+    nx = len(phi)    
+    I=0.
+    for i in range(nx):
+        I+=abs(phi[(i+1)%nx]-phi[i])
+    return I
+    
 def CTCS(phiOld, c, nt):
     
     """
@@ -29,19 +42,25 @@ def CTCS(phiOld, c, nt):
     # new time-step array for phi
     phiNew = phiOld.copy()
     phiCurrent = phiOld.copy()
+    TotalVariation = np.zeros(nt)
     
     # FTCS for computing the first time step
     for j in range(0,nx):
         phiCurrent[j] = phiOld[j] - c*0.5*(phiOld[(j+1)%nx] - phiOld[(j-1)%nx])
+    # total variation for first time step    
+    TotalVariation[0]=TV(phiCurrent)
+    
     # CTCS for all time steps
-    for it in range(nt-1):
-        
+    for it in range(1,nt):
         # spatial points
         for j in range(0,nx):
             phiNew[j] = phiOld[j] - c*\
                                    (phiCurrent[(j+1)%nx] - phiCurrent[(j-1)%nx])
+        
         # output to phiOld for the next time-step
         phiOld = phiCurrent.copy()
         phiCurrent = phiNew.copy()
+        # total variation for time steps 2 to nt
+        TotalVariation[it]=TV(phiNew)
         
-    return phiNew
+    return phiNew, TotalVariation
